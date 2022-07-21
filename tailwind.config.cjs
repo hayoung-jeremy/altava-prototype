3e5f38745a222250de659afead76c3330d2d91a2
Ha young, Kim <jeremiah91@naver.com>
2022-07-21 10:38:50 +0900

    style : fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Play: ["Play"],
+        Questrial: ["Questrial"],
+      },
+    },
   },
   plugins: [],
 }
